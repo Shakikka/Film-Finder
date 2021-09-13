1bab7fc9eb6044c6e8d10d3d0d7ec7b5b2ce5506
Joel <wilddroppings@gmail.com>
2021-09-12 23:32:26 -0600

    Map through movies and display titles

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
 import './Movies.css';
 
-const Movies = ({}) => {
-    return <p>Movies</p>
+const Movies = ({ movies }) => {
+    console.log(movies)
+    return (
+        movies.map(movie => {
+            return (
+                <Link to={`/${movie.id}`} key={movie.id} className='movie-card'>
+                    <img src='' alt=''/>
+                    {movie.title}
+                </Link>
+            )
+        })
+    )
 }
 
 export default Movies;
