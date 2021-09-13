92c9911f57d712eaeec018be61525e8a9ff65a04
Joel <wilddroppings@gmail.com>
2021-09-12 23:44:12 -0600

    Wrap movies map for better styling

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,14 +4,16 @@ import './Movies.css';
 const Movies = ({ movies }) => {
     console.log(movies)
     return (
-        movies.map(movie => {
-            return (
-                <Link to={`/${movie.id}`} key={movie.id} className='movie-card'>
-                    <img src={`/assets/moviePosterImages/${movie.id}.jpeg`} alt={`${movie.title} poster`}/>
-                    {movie.title}
-                </Link>
-            )
-        })
+        <section className='movie-cards-box'>
+            {movies.map(movie => {
+                return (
+                    <Link to={`/${movie.id}`} key={movie.id} className='movie-card'>
+                        <img src={`/assets/moviePosterImages/${movie.id}.jpeg`} alt={`${movie.title} poster`}/>
+                        {movie.title}
+                    </Link>
+                )
+            })}
+        </section>
     )
 }
 
