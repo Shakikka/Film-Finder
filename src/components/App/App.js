e87dca2049bb69d9c5ebb9a60fbf9efe2f2b94d0
Joel <wilddroppings@gmail.com>
2021-09-12 23:15:30 -0600

    Add function to retrieve and set movie data to state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,30 @@
 import { Switch, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { getMovies } from '../../apiCalls';
 import './App.css';
 
 const App = () => {
+
+  const [movies, setMovies] = useState([])
+
+
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
+  const fetchMovies = () => {
+    getMovies()
+      .then(movies => {
+        console.log(movies.data)
+        setMovies(movies.data)
+      })
+  }
+
   return (
     <div className='App'>
-      <switch>
+      <Switch>
         <Route exact path='/'/>
-      </switch>
+      </Switch>
     </div>
     )
 }
