1969210c1f4750abdc0e8e7ef90857be26e8104a
Joel <wilddroppings@gmail.com>
2021-09-13 00:32:03 -0600

    Pass through searchTerm and SetSearchTerm to searchbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import SearchBar from '../SearchBar/SearchBar';
 import './Header.css';
 
-const Header = ({}) => {
+const Header = ({ searchTerm, setSearchTerm }) => {
     return (
         <header>
             <h1>Film Finder</h1>
-            <SearchBar/>
+            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
             <img src='/android-chrome-192x192.png' alt='logo' className='logo'/>
         </header>
     )
