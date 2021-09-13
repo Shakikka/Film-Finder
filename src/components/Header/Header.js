ef2b964a4ac5fe03163c3d3c60323453e2390b91
Joel <wilddroppings@gmail.com>
2021-09-13 00:26:31 -0600

    Add dropdown menu for genres

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
+import SearchBar from '../SearchBar/SearchBar';
 import './Header.css';
 
 const Header = ({}) => {
     return (
         <header>
             <h1>Film Finder</h1>
+            <SearchBar/>
             <img src='/android-chrome-192x192.png' alt='logo' className='logo'/>
         </header>
     )
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,6 +2,33 @@ import './SearchBar.css';
 
 const SearchBar = ({}) => {
 
+
+
+
+    return (
+        <form>
+            <select className='genres'>
+                <option value=''>all genres</option>
+                <option value='action'>action</option>
+                <option value='adventure'>adventure</option>
+                <option value='animated'>animated</option>
+                <option value='biography'>biography</option>
+                <option value='children'>children</option>
+                <option value='comedy'>comedy</option>
+                <option value='comedy drama'>comedy drama</option>
+                <option value='crime drama'>crime drama</option>
+                <option value='dark comedy'>dark comedy</option>
+                <option value='drama'>drama</option>
+                <option value='fantasy'>fantasy</option>
+                <option value='historical drama'>historical drama</option>
+                <option value='horror'>horror</option>
+                <option value='mystery'>mystery</option>
+                <option value='science fiction'>science fiction</option>
+                <option value='thriller'>thriller</option>
+                <option value='western'>western</option>
+            </select>
+        </form>
+    )
 }
 
 export default SearchBar;
