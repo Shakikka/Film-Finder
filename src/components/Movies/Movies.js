0e82da015ed862bc1ed5cfd9318181c1a6503f82
Joel <wilddroppings@gmail.com>
2021-09-13 00:37:59 -0600

    Make a working search by title

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,18 +1,30 @@
 import { Link } from 'react-router-dom';
 import './Movies.css';
 
-const Movies = ({ movies }) => {
-    console.log(movies)
+const Movies = ({ movies, searchTerm }) => {
+
+    const shownMovies = movies.filter(movie => {
+        if (!searchTerm) {
+            return movie
+        } else if (movie.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return movie
+        } else {
+            return false
+        }
+    }).map(movie => {
+        return (
+            <Link to={`/${movie.id}`} key={movie.id} className='movie-card'>
+                <img src={`/assets/moviePosterImages/${movie.id}.jpeg`} alt={`${movie.title} poster`} />
+                {movie.title}
+            </Link>
+        )
+    })
+    
+    
     return (
         <section className='movie-cards-box'>
-            {movies.map(movie => {
-                return (
-                    <Link to={`/${movie.id}`} key={movie.id} className='movie-card'>
-                        <img src={`/assets/moviePosterImages/${movie.id}.jpeg`} alt={`${movie.title} poster`}/>
-                        {movie.title}
-                    </Link>
-                )
-            })}
+           {shownMovies && shownMovies}
+           {!shownMovies.length && <p>We're sorry, but there are no matches for your search.</p>}
         </section>
     )
 }
