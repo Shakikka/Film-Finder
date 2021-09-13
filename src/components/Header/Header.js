a9f7cb0c12aeb6fa2428ca3e0fdd4c5021c77e3c
Joel <wilddroppings@gmail.com>
2021-09-12 23:55:07 -0600

    Add title and icon to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import './Header';
 
 const Header = ({}) => {
-    return <p>Header</p>
+    return (
+        <header>
+            <h1>Film Finder</h1>
+            <img src='/android-chrome-192x192.png' alt='logo' className='logo'/>
+        </header>
+    )
 }
 
 export default Header;
