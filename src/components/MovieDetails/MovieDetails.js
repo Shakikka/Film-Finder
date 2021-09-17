0f81db2eee8d2d4b71203e7ece285ffc4b7f7c35
Joel <wilddroppings@gmail.com>
2021-09-16 20:55:18 -0600

    Display all movie details

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -3,17 +3,21 @@ import './MovieDetails.css';
 
 const MovieDetails = ({ movieDetails, setMovieId, id }) => {
 
+    const { description, duration, releaseDate, genres, topCast, title } = movieDetails
+
     useEffect(() => {
         setMovieId(id)
     }, [setMovieId, id])
 
     console.log('movieDetails', movieDetails)
     return (
-        <section className='movie-details' style={{backgroundImage: `url(/assets/movieHeroImages/${movieDetails.id}.jpeg)`}}>
-            <h2 className='title'>{movieDetails.title}</h2>
-            <h3>{movieDetails.description}</h3>
-            <h5>{movieDetails.duration && new Date(movieDetails.duration * 1000).toISOString().substr(11, 8)}</h5>
-            <ul>{movieDetails.topCast && movieDetails.topCast.map((member, i) => {
+        <section className='movie-details' style={{backgroundImage: `url(/assets/movieHeroImages/${id}.jpeg)`}}>
+            <h2 className='title'>{title}</h2>
+            <h3>{description}</h3>
+            <h5>{duration && new Date(duration * 1000).toISOString().substr(11, 8)}</h5>
+            <h5>{releaseDate}</h5>
+            <h5>{genres && genres.join(', ')}</h5>
+            <ul>{topCast && topCast.map((member, i) => {
                 return <li key={i + 1}>{`${member.name} as ${member.characterName}`}</li>
                 })}
             </ul>
